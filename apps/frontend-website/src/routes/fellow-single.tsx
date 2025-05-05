9dd88d0fbdef1517161b60caad25b5ff326b677b
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-05-05 15:27:09 +0530

    Single Fellow page: Improve this

--- apps/frontend-website/src/routes/fellow-single.tsx
+++ apps/frontend-website/src/routes/fellow-single.tsx
@@ -1,16 +1,9 @@
 
 import type React from "react"
 
-import {
-	isRouteErrorResponse,
-	Links,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
-} from "react-router"
 
-import type { Route } from "./+types/fellow-single"
+
+
 
 export default function ThisPage () {
 	return <div className="container md:grid-layout">
@@ -20,6 +13,8 @@ export default function ThisPage () {
 		</div>
 	</div>
 }
+
+
 
 
 
@@ -34,61 +29,81 @@ function Heading () {
 	</>
 }
 
-function Content ( { className }: React.ComponentProps<"div"> ) {
-	return <div className={ className }>
-		<div className="md:flex md:gap-1g">
-			<div>
-				<figure className="max-md:size-[--content-width] rounded-md overflow-hidden">
-					<img src="/media/rahul-bhusan.png" alt="Rahul Bhusan" className="md:w-2c-1g lg:w-3c-2g md:aspect-square object-cover" />
-				</figure>
-			</div>
-			<div className="mt-4 md:mt-0 md:w-5c-4g lg:w-6c-5g | space-y-8 lg:space-y-10">
-				<p className="text-p">NORTH is driven by the profound sense of responsibility its architects and artists feel, to safeguard the wisdom of their homeland and to create meaningful spaces which enable conscious living. The collective designs and builds structures, conducts experiential workshops, and hosts people through regenerative travel at their campus in Naggar, Himachal Pradesh.</p>
+function Content ( { className }: React.ComponentProps<"main"> ) {
+	return <main className={ className }>
+		<IntroSection className="md:flex md:gap-1g" />
 
-				<p className="text-p">Based upon a decade of on-the-ground research and documentation into centuries-old vernacular buildings and the cultures that surround them, NORTH’s design practice is rooted in research, innovation, and practical exploration. On campus is a diverse team of engineers, designers, architects, craftspeople and artists, all on a journey of learning-by-making, continually experimenting with traditional building techniques and natural materials to harmonize with today’s culture, while also nodding to the future.</p>
+		<TestimonialSection className="mt-6 md:mt-8 lg:mt-10 | space-y-6 md:space-y-8 lg:space-y-10" />
 
-				<p className="text-p">NORTH sees sustainability as a fundamental quality, core to every action. Every project starts with a simple question: “why?” That question drives humility, an openness to learn from nature, and an insistence to build with intention.</p>
+		<FellowsWordsSection className="mt-6 md:mt-8 lg:mt-10 | space-y-6 md:space-y-8 lg:space-y-10" />
 
-				<p className="text-p">Through the Fellowship with Godrej Design Lab, NORTH’s founder, Rahul Bushan, has started a larger project to collect his design insights and life lessons into a book. While the story may be personal and specific, the lessons are highly relevant to a world in search of sustainable solutions and meaningful living.</p>
+		<CreditsSection />
+	</main>
+}
 
-				<p className="text-p">“These learnings and lessons come from my natural building practice, from growing up in small mountain villages in the Himalayas and from being the black sheep of a family who has worked government jobs for generations — only to start my own business as a creative. Through the lens of my journey, the book will share mountain living wisdom and how to apply the lessons of vernacular architecture in a practical way. It will hopefully be used as a creative guide for entrepreneurs, young architects and people who strive to go above and beyond. It is a journey on how to live a life with purpose.”</p>
-			</div>
+
+function IntroSection ( { className }: React.ComponentProps<"section"> ) {
+	return <section className={ className }>
+		<div>
+			<figure className="max-md:size-[--content-width] rounded-md overflow-hidden">
+				<img src="/media/rahul-bhusan.png" alt="Rahul Bhusan" className="md:w-2c-1g lg:w-3c-2g md:aspect-square object-cover" />
+			</figure>
 		</div>
 
-		<div className="mt-6 md:mt-8 lg:mt-10 | space-y-6 md:space-y-8 lg:space-y-10">
-			<hr className="border-black" />
+		<div className="mt-4 md:mt-0 md:w-5c-4g lg:w-6c-5g | space-y-8 lg:space-y-10">
+			<p className="text-p">NORTH is driven by the profound sense of responsibility its architects and artists feel, to safeguard the wisdom of their homeland and to create meaningful spaces which enable conscious living. The collective designs and builds structures, conducts experiential workshops, and hosts people through regenerative travel at their campus in Naggar, Himachal Pradesh.</p>
 
-			<h3 className="text-h4 md:text-h2 lg:text-h4 font-bold uppercase text-primary">From the GDL Team</h3>
+			<p className="text-p">Based upon a decade of on-the-ground research and documentation into centuries-old vernacular buildings and the cultures that surround them, NORTH’s design practice is rooted in research, innovation, and practical exploration. On campus is a diverse team of engineers, designers, architects, craftspeople and artists, all on a journey of learning-by-making, continually experimenting with traditional building techniques and natural materials to harmonize with today’s culture, while also nodding to the future.</p>
 
-			<p className="font-mono text-p font-bold">Rahul’s passion is clear – he is driven by an intense curiosity in how environment and design combine to create, shape, and reflect culture. In channeling this passion, Rahul has been able to muster a highly creative and talented team to not just preserve vernacular architecture in amber, but to understand how it actively works with the environment, culture, living and then to recontextualize it into projects which are built for today and the future.</p>
+			<p className="text-p">NORTH sees sustainability as a fundamental quality, core to every action. Every project starts with a simple question: “why?” That question drives humility, an openness to learn from nature, and an insistence to build with intention.</p>
 
-			<p className="font-mono text-p font-bold">In an industry and a place grappling with the push-and-pull of “past” and “future,” “organic” and “synthetic,” “urban” and “rural,” we were excited to be part of a project which aims to explore, uncover, and share how a young architect has experienced and responded to this context.</p>
+			<p className="text-p">Through the Fellowship with Godrej Design Lab, NORTH’s founder, Rahul Bushan, has started a larger project to collect his design insights and life lessons into a book. While the story may be personal and specific, the lessons are highly relevant to a world in search of sustainable solutions and meaningful living.</p>
+
+			<p className="text-p">“These learnings and lessons come from my natural building practice, from growing up in small mountain villages in the Himalayas and from being the black sheep of a family who has worked government jobs for generations — only to start my own business as a creative. Through the lens of my journey, the book will share mountain living wisdom and how to apply the lessons of vernacular architecture in a practical way. It will hopefully be used as a creative guide for entrepreneurs, young architects and people who strive to go above and beyond. It is a journey on how to live a life with purpose.”</p>
 		</div>
+	</section>
+}
 
-		<div className="mt-6 md:mt-8 lg:mt-10 | space-y-6 md:space-y-8 lg:space-y-10">
-			<hr className="border-black" />
+function TestimonialSection ( { className }: React.ComponentProps<"section"> ) {
+	return <section className={ className }>
+		<hr className="border-black" />
 
-			<h2 className="text-h1 font-bold uppercase">
-				<span className="text-primary">About the Book</span>
-				<br />
-				<span className="text-secondary">in my own words</span>
-			</h2>
+		<h3 className="text-h4 md:text-h2 lg:text-h4 font-bold uppercase text-primary">From the GDL Team</h3>
 
-			<div className="md:ml-2c-2g md:w-5c-4g lg:ml-3c-3g lg:w-6c-5g | space-y-6 md:space-y-8">
-				<p className="text-p">What does it mean to build something lasting— not just in stone or wood, but in values, culture, and purpose? Why do stories of belonging captivate and ground us in something larger than ourselves?</p>
+		<p className="font-mono text-p font-bold">Rahul’s passion is clear – he is driven by an intense curiosity in how environment and design combine to create, shape, and reflect culture. In channeling this passion, Rahul has been able to muster a highly creative and talented team to not just preserve vernacular architecture in amber, but to understand how it actively works with the environment, culture, living and then to recontextualize it into projects which are built for today and the future.</p>
 
-				<p className="text-p">Born in the mountains of Himachal Pradesh, this book is an answer to these questions. A journey through mountains and memories, architecture and artistry, culture and community. It is my testament to how ancient wisdom and modern vision can come together to craft something transformative.</p>
+		<p className="font-mono text-p font-bold">In an industry and a place grappling with the push-and-pull of “past” and “future,” “organic” and “synthetic,” “urban” and “rural,” we were excited to be part of a project which aims to explore, uncover, and share how a young architect has experienced and responded to this context.</p>
+	</section>
+}
 
-				<p className="text-p">At its heart, the book is a modest endeavour to share the lessons, values and wisdom that living in the ‘Valley of Gods’ is teaching me. From these principles, my architectural practice has formed a tentative voice, my research has found meaning outside written pages into reality as natural homes, and every day is filled with purpose to build a life and a community, rooted in coexistence.</p>
+function FellowsWordsSection ( { className }: React.ComponentProps<"section"> ) {
+	return <section className={ className }>
+		<hr className="border-black" />
 
-				<p className="text-p">Inside the current manuscript, you will find vernacular architecture research, Himachali craft documentation, on-site experiments with natural buildings, illustrations of mountain life and beyond, intangible cultural heritage, age-old transcribed and translated folk songs, mystical oral stories passed down from generations, a life-changing trek studying high altitude roots and plants used for medicinal and shamanistic purposes, detailed architectural drawings, sketches of tools and objects worn with time—all bound together into a single, tumultuous and passion-filled narrative.</p>
+		<h2 className="text-h1 font-bold uppercase">
+			<span className="text-primary">About the Book</span>
+			<br />
+			<span className="text-secondary">in my own words</span>
+		</h2>
 
-				<p className="text-p">Not just a memoir or a technical manual—it is becoming an experiential guide for dreamers and doers. For entrepreneurs, it offers lessons on creating purpose-driven businesses, rooted in authenticity rather than the rush of commercialism. For architects, it presents a new outlook, encouraging them to find their niche in a saturated world. And for creatives, it shows how one’s roots can become the foundation for something monumental.</p>
+		<div className="md:ml-2c-2g md:w-5c-4g lg:ml-3c-3g lg:w-6c-5g | space-y-6 md:space-y-8">
+			<p className="text-p">What does it mean to build something lasting— not just in stone or wood, but in values, culture, and purpose? Why do stories of belonging captivate and ground us in something larger than ourselves?</p>
 
-				<p className="text-p">It hopes to be an experience, a guide, a piece of living history that motivates us to find the answers and a more fulfilling life deep within our roots.</p>
-			</div>
+			<p className="text-p">Born in the mountains of Himachal Pradesh, this book is an answer to these questions. A journey through mountains and memories, architecture and artistry, culture and community. It is my testament to how ancient wisdom and modern vision can come together to craft something transformative.</p>
+
+			<p className="text-p">At its heart, the book is a modest endeavour to share the lessons, values and wisdom that living in the ‘Valley of Gods’ is teaching me. From these principles, my architectural practice has formed a tentative voice, my research has found meaning outside written pages into reality as natural homes, and every day is filled with purpose to build a life and a community, rooted in coexistence.</p>
+
+			<p className="text-p">Inside the current manuscript, you will find vernacular architecture research, Himachali craft documentation, on-site experiments with natural buildings, illustrations of mountain life and beyond, intangible cultural heritage, age-old transcribed and translated folk songs, mystical oral stories passed down from generations, a life-changing trek studying high altitude roots and plants used for medicinal and shamanistic purposes, detailed architectural drawings, sketches of tools and objects worn with time—all bound together into a single, tumultuous and passion-filled narrative.</p>
+
+			<p className="text-p">Not just a memoir or a technical manual—it is becoming an experiential guide for dreamers and doers. For entrepreneurs, it offers lessons on creating purpose-driven businesses, rooted in authenticity rather than the rush of commercialism. For architects, it presents a new outlook, encouraging them to find their niche in a saturated world. And for creatives, it shows how one’s roots can become the foundation for something monumental.</p>
+
+			<p className="text-p">It hopes to be an experience, a guide, a piece of living history that motivates us to find the answers and a more fulfilling life deep within our roots.</p>
 		</div>
+	</section>
+}
 
+function CreditsSection ( { className }: React.ComponentProps<"section"> ) {
+	return <section className={ className }>
 		<div className="mt-6 md:mt-8 lg:mt-10 | space-y-6 md:space-y-8 lg:space-y-10">
 			<hr className="border-black" />
 
@@ -178,5 +193,5 @@ function Content ( { className }: React.ComponentProps<"div"> ) {
 				</div>
 			</div>
 		</div>
-	</div>
+	</section>
 }
