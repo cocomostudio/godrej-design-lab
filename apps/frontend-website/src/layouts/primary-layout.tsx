d0300e6479c4f4a751dc3b3e0596ca1fc35e7e19
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-05-21 21:56:43 +0530

    Frontend website: Page header: Improve the menu expanding transition

--- apps/frontend-website/src/layouts/primary-layout.tsx
+++ apps/frontend-website/src/layouts/primary-layout.tsx
@@ -29,9 +29,9 @@ export default function ThisLayout () {
 function Header () {
 	const [ isNavOpen, setIsNavOpen ] = useState( false )
 	return <header>
-		<div className={ `relative z-10 grid ${ isNavOpen ? "grid-rows-[1fr]" : "grid-rows-[0fr]" } transition-all duration-450 ease-vaul` }>
+		<div className={ `relative z-10 grid ${ isNavOpen ? "not-interpolate:grid-rows-[1fr] interpolate:h-auto" : "not-interpolate:grid-rows-[0fr] interpolate:h-0" } not-interpolate:transition-all interpolate:transition-[height] not-interpolate:will-change-[grid-rows] interpolate:will-change-[height] !duration-450 !ease-vaul` }>
 			<div className="overflow-hidden">
-				<HeaderNavigation isVisible={ isNavOpen } className={ `w-full transition-transform duration-450 ease-vaul ${ isNavOpen ? "translate-y-0" : "-translate-y-full" }` } />
+				<HeaderNavigation isVisible={ isNavOpen } className={ `w-full transition-transform duration-450 ease-vaul ${ isNavOpen ? "translate-y-0" : "-translate-y-full pointer-events-none" }` } />
 			</div>
 		</div>
 		<div className="relative container flex justify-between items-start pt-8">
@@ -55,17 +55,17 @@ function HeaderNavigation ( { isVisible = false, className = "" }: HeaderNavigat
 		<nav className="md:container md:grid-layout">
 			<div className="end-col-1 text-secondary">
 				<div className="container flex justify-between items-start pt-8">
-					<Link to={ "/" } className={ `transition-opacity duration-250 delay-200 ease-out ${ isVisible ? 'opacity-100' : 'opacity-0' }` }>
+					<Link to={ "/" } className={ `transition-opacity duration-250 ease-out ${ isVisible ? 'opacity-100 delay-300' : 'opacity-0' }` }>
 						<GDLLogo className="w-1c md:w-[calc(var(--column-width)/2)] lg:w-[calc(3*(var(--column-width)/4))] h-auto fill-secondary" />
 					</Link>
-					<div className={ `absolute top-full left-0 w-full transition-opacity duration-150 delay-200 ease-out ${ isVisible ? "opacity-100" : "opacity-0" }` }>
+					<div className={ `absolute top-full left-0 w-full transition-opacity duration-150 ease-out ${ isVisible ? "opacity-100 delay-300" : "opacity-0" }` }>
 						<div className="container text-right">
 							<button type="button" className="text-sm font-bold uppercase">Close</button>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div className={ `start-col-2 end-gutter-5 lg:end-col-7 text-secondary transition-opacity duration-250 delay-200 ease-out ${ isVisible ? "opacity-100" : "opacity-0" }` }>
+			<div className={ `start-col-2 end-gutter-5 lg:end-col-7 text-secondary transition-opacity duration-250 ease-out ${ isVisible ? "opacity-100 delay-300" : "opacity-0" }` }>
 				<ul className="mt-8 container flex flex-col md:flex-row flex-wrap gap-6 pb-6 md:gap-3 lg:gap-1g md:pb-0">
 					<li className="md:w-1c-3g lg:w-2c-1g border-t border-secondary border-solid pt-6 md:pt-3"><Link to={ "#" } className="text-p md:text-sm font-bold uppercase">About</Link></li>
 					<li className="md:w-1c-3g lg:w-2c-1g border-t border-secondary border-solid pt-6 md:pt-3"><Link to={ "#" } className="text-p md:text-sm font-bold uppercase">Fellowship</Link></li>
@@ -77,8 +77,8 @@ function HeaderNavigation ( { isVisible = false, className = "" }: HeaderNavigat
 			</div>
 			<div className="start-col-6 end-col-last md:ml-1g md:pl-6 lg:start-col-8 lg:ml-[calc(var(--column-width)/2)] bg-secondary text-primary z-10">
 				<div className="container py-8 bg-secondary">
-					<h2 className={ `border-t border-primary border-solid md:border-none pt-4 md:pt-0 text-p font-bold uppercase transition-opacity duration-250 delay-200 ease-out ${ isVisible ? "opacity-100" : "opacity-0" }` }>Latest</h2>
-					<ul className={ `mt-6 space-y-3 transition-opacity duration-250 delay-200 ease-out ${ isVisible ? "opacity-100" : "opacity-0" }` }>
+					<h2 className={ `border-t border-primary border-solid md:border-none pt-4 md:pt-0 text-p font-bold uppercase transition-opacity duration-250 ease-out ${ isVisible ? "opacity-100 delay-300" : "opacity-0" }` }>Latest</h2>
+					<ul className={ `mt-6 space-y-3 transition-opacity duration-250 ease-out ${ isVisible ? "opacity-100 delay-300" : "opacity-0" }` }>
 						<li className=""><Link to={ "#" } className="text-sm">Myth and lore in architecture with Bijoy Jain &amp; Studio Mumbai</Link></li>
 						<li className=""><Link to={ "#" } className="text-sm">Understanding conscious living in India: Materials &amp; processes</Link></li>
 					</ul>
