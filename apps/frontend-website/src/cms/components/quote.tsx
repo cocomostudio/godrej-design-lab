1069a58c1e14ac9a697b72ce823d2fea55b53ad7
thehassanshaikh <thehasanshaikh@gmai.com>
2025-05-31 01:51:24 +0530

    added the qoute component

--- apps/frontend-website/src/cms/components/quote.tsx
+++ apps/frontend-website/src/cms/components/quote.tsx
@@ -18,34 +18,34 @@ export class Quote {
 		return shallow_clone_props(props);
 	}
 
-static Renderer(props) {
-	console.log(props.content);
-  return (
-    <blockquote>
-      <p>
-        <BlocksRenderer
-          content={props.content}
-          blocks={{
-            paragraph: ({ children }) => <>{children}</>,
-            heading: ({ children }) => <>{children}</>,
-            link: ({ children, url }) => <a href={url}>{children}</a>,
-            quote: ({ children }) => <>{children}</>,
-            code: ({ children }) => <>{children}</>,
-            image: () => null, // skip image rendering inside text
-						
-            // Render lists inline with no bullets/numbers and no extra spans
-            list: ({ children }) => <>{children}</>,
+	static Renderer(props) {
+		console.log(props.content);
+		return (
+			<blockquote className="md:ml-1c-1g md:w-6c-5g lg:w-8c-7g flex items-center h-[660px]">
+				<p className="text-h1 font-bold md:leading-[1.1] text-red">
+					<BlocksRenderer
+						content={props.content}
+						blocks={{
+							paragraph: ({ children }) => <>{children} </>,
+							heading: ({ children }) => <>{children}</>,
+							link: ({ children, url }) => <a href={url}>{children} </a>,
+							quote: ({ children }) => <>{children} </>,
+							code: ({ children }) => <>{children} </>,
+							image: () => null, // skip image rendering inside text
 
-            // Render list items directly (no <li>)
-            "list-item": ({ children }) => <>{children}</>
-          }}
-          modifiers={{
-            bold: ({ children }) => <strong>{children}</strong>,
-            italic: ({ children }) => <span className="italic">{children}</span>,
-          }}
-        />
-      </p>
-    </blockquote>
-  );
-}
+							// Render lists inline with no bullets/numbers and no extra spans
+							list: ({ children }) => <>{children}</>,
+
+							// Render list items directly (no <li>)
+							"list-item": ({ children }) => <>{children} </>
+						}}
+						modifiers={{
+							bold: ({ children }) => <strong>{children}</strong>,
+							italic: ({ children }) => <span className="italic">{children}</span>,
+						}}
+					/>
+				</p>
+			</blockquote>
+		);
+	}
 }
