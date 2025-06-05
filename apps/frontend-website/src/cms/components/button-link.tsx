698c936f1ee033a236e29d8e7c418c4a54f929b6
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-06-05 17:55:05 +0530

    Button Link component: Label attribute: Remove it as it is redundant

--- apps/frontend-website/src/cms/components/button-link.tsx
+++ apps/frontend-website/src/cms/components/button-link.tsx
@@ -17,12 +17,14 @@ export class ButtonLink {
 		return shallow_clone_props( props )
 	}
 
-	static Renderer ({ text, link }) {
+	static Renderer ({ link }) {
 		const className = "[p+&]:mt-2"
 		let props = { }
 		if ( ! link.url.startsWith( "/" ) ) {
 			props.target = "_blank"
 		}
-		return <Link to={ link.url } className={ `${ className } inline-block rounded-md bg-secondary text-primary px-3 py-3.5 md:px-3.75 md:py-2.25 lg:px-6 lg:py-3.75 text-xs lg:text-sm uppercase` } { ...props }>{ text }</Link>
+		return <Link to={ link.url } className={ `${ className } inline-block rounded-md bg-secondary text-primary px-3 py-3.5 md:px-3.75 md:py-2.25 lg:px-6 lg:py-3.75 text-xs lg:text-sm uppercase` } { ...props }>
+			{ link.label }
+		</Link>
 	}
 }
