7411787c3f87e85b014a986b85a4c5db0712d22b
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-06-05 18:09:41 +0530

    CMS Routes: Page Contexts: Detach the routes from the hard-coded page contexts

--- apps/frontend-website/src/routes.ts
+++ apps/frontend-website/src/routes.ts
@@ -9,11 +9,11 @@ import {
 
 export default [
 	index( "routes/home.tsx" ),
+	route( "*", "cms/cms-route-handler.tsx" ),
 	layout( "./layouts/primary-layout.tsx", [
 		route( "fellows/rahul-bhusan", "routes/fellow-single.tsx" ),
 		route( "kitchen-sink", "routes/kitchen-sink.tsx" ),
 		route( "about", "routes/about.tsx" ),
 		route( "about-fellowship", "routes/about-fellowship.tsx" ),
-		route( "*", "cms/cms-route-handler.tsx" ),
 	] )
 ] satisfies RouteConfig
