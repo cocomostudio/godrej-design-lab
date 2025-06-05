fa421455c475f2e7ae12fdc94d79b001dc2462dc
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-06-05 18:18:45 +0530

    Strapi: Admin Dashboard: Support previewing content within Strapi itself

--- apps/cms/config/admin.ts
+++ apps/cms/config/admin.ts
@@ -17,5 +17,31 @@ export default function ( { env } ) {
 			promoteEE: env.bool( "FLAG_PROMOTE_EE", false ),
 		},
 		autoOpen: false,
+		preview: {
+			enabled: true,
+			config: {
+				allowedOrigins: env( "CLIENT_URL" ),
+				async handler ( uid, { documentId, locale, status } ) {
+					if ( ! uid || ! status ) {
+						return null
+					}
+
+					const document = await strapi.documents( uid ).findOne( {
+						documentId,
+						status,
+						populate: [
+							"url_alias"
+						]
+					} )
+
+					const url_path = document?.url_alias?.[ 0 ]?.url_path
+					if ( ! url_path ) {
+						return null
+					}
+
+					return env( "CLIENT_URL" ) + url_path
+				}
+			}
+		},
 	}
 }
