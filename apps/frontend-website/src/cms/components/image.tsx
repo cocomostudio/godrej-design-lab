a5916d0949885e23c74c54b42834bedbb7f3dfcb
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-06-05 18:01:01 +0530

    Image component: Add an aspect-ratio attribute

--- apps/frontend-website/src/cms/components/image.tsx
+++ apps/frontend-website/src/cms/components/image.tsx
@@ -17,9 +17,14 @@ export class Image {
 		return shallow_clone_props( props )
 	}
 
-	static Renderer ({ file }) {
-		return <figure className="rounded-md overflow-hidden">
-			<img src={ CMS_PUBLIC_DIR_URL + file.url } alt={ file.alternativeText } className="md:aspect-square object-cover" />
+	static Renderer ({ file, aspect_ratio }) {
+		return <figure className="mt-6 md:mt-8 lg:mt-10 rounded-md overflow-hidden">
+			<img src={ CMS_PUBLIC_DIR_URL + file.url } alt={ file.alternativeText } className={ `${ aspect_ratio_to_class__map[ aspect_ratio ] } object-cover` } loading="lazy" />
 		</figure>
 	}
+}
+
+const aspect_ratio_to_class__map = {
+	"natural": "",
+	"1:1 (square)": "aspect-square",
 }
