06c3a11762d3c425cda8b305abee141f9b9b8f1f
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-06-05 17:59:04 +0530

    Columns: Flesh out the styles for the other column ratios

--- apps/frontend-website/src/cms/components/column.tsx
+++ apps/frontend-website/src/cms/components/column.tsx
@@ -42,7 +42,17 @@ const column_config_to_classname = {
 	},
 	"3:4": {
 		"1": "md:w-3c-2g lg:w-4c-3g",
-		"1_empty": "md:ml-3c-2g lg:ml-4c-3g",
+		"1_empty": "md:ml-3c-3g lg:ml-4c-4g",
 		"2": "md:ml-1g md:w-4c-3g lg:w-5c-4g",
+	},
+	"2:7": {
+		"1": "md:w-2c-1g lg:w-2c-1g",
+		"1_empty": "md:ml-2c-2g lg:ml-2c-2g",
+		"2": "md:ml-1g md:w-5c-4g lg:w-7c-6g",
+	},
+	"7:2": {
+		"1": "md:w-5c-4g lg:w-7c-6g",
+		"1_empty": "md:ml-5c-5g lg:ml-7c-7g",
+		"2": "md:ml-1g md:w-2c-1g lg:w-2c-1g",
 	},
 }
