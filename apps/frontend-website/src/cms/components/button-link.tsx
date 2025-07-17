6c6e3e68639bcb70080c56569d51a422171ca69d
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-07-17 17:49:04 +0530

    Button Link component: Allow the target attribute to be overridden

--- apps/frontend-website/src/cms/components/button-link.tsx
+++ apps/frontend-website/src/cms/components/button-link.tsx
@@ -17,12 +17,16 @@ export class ButtonLink {
 		return shallow_clone_props( props )
 	}
 
-	static Renderer ( { link, className }: { link: unknown, className?: string } ) {
+	static Renderer ( { link, target, className }: { link: unknown, target?: Parameters<typeof Link>[ 0 ][ "target" ], className?: string } ) {
 		const classes = "[p+&]:mt-2"
 		let props = { }
-		if ( ! link.url.startsWith( "/" ) ) {
+		if ( target ) {
+			props.target = target
+		}
+		else if ( ! link.url.startsWith( "/" ) ) {
 			props.target = "_blank"
 		}
+
 		return <Link to={ link.url } className={ `${ classes } inline-block rounded-md bg-secondary text-primary px-3 py-3.5 md:px-3.75 md:py-2.25 lg:px-6 lg:py-3.75 text-xs lg:text-sm uppercase ${ className }` } { ...props }>
 			{ link.label }
 		</Link>
