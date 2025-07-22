0a468785206f82a5ae3287860920a5724b3a2304
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-07-22 16:50:41 +0530

    Routes: Move the static version of the home page to another URL

--- apps/frontend-website/src/routes.ts
+++ apps/frontend-website/src/routes.ts
@@ -17,6 +17,6 @@ export default [
 		route( "kitchen-sink", "routes/kitchen-sink.tsx" ),
 		route( "about", "routes/about.tsx" ),
 		route( "about-fellowship", "routes/about-fellowship.tsx" ),
-		route( "home", "routes/home-dev.tsx" ),
+		route( "home-dev", "routes/home-dev.tsx" ),
 	] ),
 ] satisfies RouteConfig
