6a12c324f7198390f8ca195344f15f8cdd25ccf6
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-07-22 16:26:58 +0530

    Heading component: Refactor code

--- apps/frontend-website/src/cms/components/heading.tsx
+++ apps/frontend-website/src/cms/components/heading.tsx
@@ -28,6 +28,7 @@ export class Heading {
 
 	static Renderer ({ level, line_1 = "", line_2, font_family, link, className = null, children = null, style }) {
 		const font_family_class = font_family === "monospace" ? "font-mono" : "font-sans"
+		const common_classes = "full-width h mt-6 md:mt-8 lg:mt-10"
 		let classes = heading_levels_to_classes[ level ]
 
 		if ( className && className.includes( "normal-case" ) ) {
@@ -39,14 +40,14 @@ export class Heading {
 
 		if ( children ) {
 			return <HeadingContainer link={ link }>
-				<H className={ `h mt-6 md:mt-8 lg:mt-10 ${ classes } text-primary ${ className }` } style={ style }>
+				<H className={ `${ common_classes } ${ classes } text-primary ${ className }` } style={ style }>
 					{ children }
 				</H>
 			</HeadingContainer>
 		}
 		else if ( line_1 ) {
 			return <HeadingContainer link={ link }>
-				<H className={ `h mt-6 md:mt-8 lg:mt-10 ${ classes } ${ font_family_class } ${ className }` } style={ style }>
+				<H className={ `${ common_classes } ${ classes } ${ font_family_class } ${ className }` } style={ style }>
 					<span className="text-primary">{ line_1 }</span>
 					<br />
 					<span className="text-secondary">{ line_2 }</span>
@@ -55,7 +56,7 @@ export class Heading {
 		}
 		else {
 			return <HeadingContainer link={ link }>
-				<H className={ `h mt-6 md:mt-8 lg:mt-10 ${ classes } text-secondary ${ className }` } style={ style }>
+				<H className={ `${ common_classes } ${ classes } text-secondary ${ className }` } style={ style }>
 					{ line_2 }
 				</H>
 			</HeadingContainer>
