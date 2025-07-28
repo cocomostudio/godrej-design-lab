1ddb7c944db87cfc52ef32824df89993abd3acfb
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-07-28 22:37:01 +0530

    Promo component: Add a gradient overlay

--- apps/frontend-website/src/cms/components/gdl/promo.tsx
+++ apps/frontend-website/src/cms/components/gdl/promo.tsx
@@ -22,6 +22,7 @@ export class Promo {
 	static Renderer ({ heading, image, link, content }) {
 		return <div className="relative mt-6 md:mt-8 lg:mt-10 md:w-7c-6g lg:w-9c-8g p-6 md:px-0 md:py-50 rounded-xl overflow-hidden">
 			<Image.Renderer file={ image } className="decorative absolute inset-0 !m-0 [&_img]:size-full" />
+			<div className="absolute inset-0 mix-blend-multiply" style={{ backgroundImage: `linear-gradient( 186deg, #FFFFFF -3.12%, #6A6A67 88.6% )` }} aria-hidden={ true } />
 			<div className="relative flex flex-col _justify-center md:flex-row md:items-center md:ml-1c-1g md:w-5c-4g lg:w-7c-6g">
 				<CoerceToHeading heading={ heading } className="md:w-3c-2g lg:w-4c-3g" />
 				<div className="mt-33 md:mt-0 md:ml-1g _lg:ml-2c-1g md:w-2c-1g lg:w-3c-2g">
