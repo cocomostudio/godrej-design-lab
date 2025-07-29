a5943576b9dcc01286f03df012b5eab69ecd2057
thehassanshaikh <thehasanshaikh@gmai.com>
2025-07-30 01:39:27 +0530

    worked on hideing the progress bar if nav is open

--- apps/frontend-website/src/__lib/this/ui/components/PageScrollProgressIndicator.tsx
+++ apps/frontend-website/src/__lib/this/ui/components/PageScrollProgressIndicator.tsx
@@ -1,32 +1,34 @@
 import React, { useEffect, useState } from 'react';
 
-export const ScrollProgressBar = () => {
-  const [scrollProgress, setScrollProgress] = useState(0);
+export const ScrollProgressBar = ({ hide = false }: { hide?: boolean }) => {
+	const [scrollProgress, setScrollProgress] = useState(0);
 
-  const handleScroll = () => {
-    const scrollTop =
-      document.documentElement.scrollTop || document.body.scrollTop;
-    const scrollHeight =
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight;
+	const handleScroll = () => {
+		const scrollTop =
+			document.documentElement.scrollTop || document.body.scrollTop;
+		const scrollHeight =
+			document.documentElement.scrollHeight -
+			document.documentElement.clientHeight;
 
-    const scrolled = scrollTop / scrollHeight;
-    setScrollProgress(scrolled);
-  };
+		const scrolled = scrollTop / scrollHeight;
+		setScrollProgress(scrolled);
+	};
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true });
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll, { passive: true });
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
 
-  return (
-    <div className="fixed top-0 left-0 w-full h-1 bg-transparent z-50">
-      <div
-        className="h-full bg-green-500 origin-left transition-transform duration-200 ease-out"
-        style={{
-          transform: `scaleX(${scrollProgress})`,
-        }}
-      ></div>
-    </div>
-  );
+	if (hide) return null;
+
+	return (
+		<div className="fixed top-0 left-0 w-full h-1 bg-transparent z-50">
+			<div
+				className="h-full bg-blue-500 origin-left transition-transform duration-200 ease-out"
+				style={{
+					transform: `scaleX(${scrollProgress})`,
+				}}
+			></div>
+		</div>
+	);
 };
