d20adaaa58e1210587b21937344eb2a81e6e5b3d
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-07-29 22:45:17 +0530

    Color Scheme: Update the `useColorSchemeStyles` hook

--- apps/frontend-website/src/cms/components/page-layout/use-color-scheme-styles.ts
+++ apps/frontend-website/src/cms/components/page-layout/use-color-scheme-styles.ts
@@ -1,19 +1,20 @@
 
+type RGBString = `${ number }, ${ number }, ${ number }`
 type Params = {
-	colors: `${ string } / ${ string }`
+	primary_color_rgb: RGBString;
+	secondary_color_rgb: RGBString;
 }
 interface ReturnType extends React.CSSProperties {
-	"--primary-color": `var( ${ string } )`;
-	"--secondary-color": `var( ${ string } )`;
+	"--primary-color": RGBString;
+	"--secondary-color": RGBString;
 }
 export function useColorSchemeStyles ( color_scheme: Params ): ReturnType | undefined {
 	if ( ! color_scheme ) {
 		return
 	}
 
-	const [ primary_color, secondary_color ] = color_scheme.colors.split( " / " )
 	return {
-		"--primary-color": `var( --${ primary_color } )`,
-		"--secondary-color": `var( --${ secondary_color } )`,
+		"--primary-color": color_scheme.primary_color_rgb,
+		"--secondary-color": color_scheme.secondary_color_rgb,
 	}
 }
