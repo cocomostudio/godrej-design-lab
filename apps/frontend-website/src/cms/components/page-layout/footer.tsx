12d678c2164d6beb9ba9f606c8387f2d9a76b6e8
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-07-30 20:02:41 +0530

    Frontend website: Footer: Improve and fix a bunch of things

--- apps/frontend-website/src/cms/components/page-layout/footer.tsx
+++ apps/frontend-website/src/cms/components/page-layout/footer.tsx
@@ -19,19 +19,19 @@ export function Footer ( { navigation, className }: FooterNavigationProps ) {
 						</div>
 					</div>
 					<div className="mt-10 md:mt-15.5 lg:mt-28 text-white">
-						<ul className="flex flex-col md:flex-row flex-wrap gap-1g pb-4 md:pb-0 md:pr-8 lg:pr-12 | text-p md:text-sm font-bold uppercase" style={{ "--md-nav-w": "calc( ( 1.5 * var( --column-width ) ) + var( --gutter-width ) )" }}>
-							{ navigation.map( ( { label, url }, i ) => <li key={ i } className="md:w-[--md-nav-w] lg:w-2c-3g border-t border-secondary border-solid pt-4.5 md:pt-3"><Link to={ url }>{ label }</Link></li> ) }
+						<ul className="flex flex-col md:flex-row flex-wrap gap-1g pb-4 md:pb-0 md:pr-8 lg:pr-12 | text-p md:text-sm font-bold uppercase">
+							{ navigation.map( ( { label, url }, i ) => <li key={ i } className="md:w-[calc((100%-(2*var(--gutter-width)))/3)] border-t border-secondary border-solid pt-4.5 md:pt-3"><Link to={ url }>{ label }</Link></li> ) }
 						</ul>
 					</div>
 				</div>
-				<div className="relative start-gutter-5 lg:start-col-8 end-col-last lg:ml-0 py-4.5 md:pl-8 md:py-6 lg:pl-12 lg:py-10 bg-secondary text-white z-10 | before:absolute before:top-0 before:right-full before:w-1/2 before:h-full before:bg-secondary md:before:hidden after:absolute after:top-0 after:left-full after:w-1/2 after:h-full after:bg-secondary md:after:hidden">
+				<div className="relative start-gutter-5 lg:start-gutter-7 end-col-last lg:ml-0 py-4.5 md:pl-8 md:py-6 lg:pl-12 lg:py-10 bg-secondary text-white z-10 | before:absolute before:top-0 before:right-full before:w-1/2 before:h-full before:bg-secondary md:before:hidden after:absolute after:top-0 after:left-full after:w-1/2 after:h-full after:bg-secondary md:after:hidden">
 					<div className="md:flex flex-col justify-between h-full bg-secondary">
 						<Link to={ "/" }>
 							<GodrejLogo className="md:w-1c h-auto fill-white" />
 						</Link>
 						<div className="mt-10">
 							<p className="md:mt-auto font-mono text-2xs md:text-sm/[normal] font-bold">Copyright © Godrej Enterprises Group. All rights reserved</p>
-							<ul className="mt-4 md:mt-3 flex justify-between | font-mono text-2xs md:text-sm font-bold">
+							<ul className="mt-4 md:mt-3 flex md:max-lg:flex-wrap justify-between | font-mono text-2xs md:text-sm font-bold">
 								<li><Link to="#">Legal</Link></li>
 								<li><Link to="#">Disclaimer</Link></li>
 								<li><Link to="/privacy-policy">Privacy Policy</Link></li>
