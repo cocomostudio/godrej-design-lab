9a5d8cd1be2df03664be703b94bba2ca52891b83
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-07-30 19:47:06 +0530

    Button: Text: Make the text bold

--- apps/frontend-website/src/__lib/react/button.tsx
+++ apps/frontend-website/src/__lib/react/button.tsx
@@ -26,7 +26,7 @@ const Button = <C extends ElementType = typeof default_element>( {
 	const Component = as || default_element
 
 	// Combine all classNames
-	const combined_class_name = `inline-block rounded-md bg-secondary text-white px-3 py-3.5 md:px-3.75 md:py-2.25 lg:px-6 lg:py-3.75 text-xs lg:text-sm uppercase ${ className }`
+	const combined_class_name = `inline-block rounded-md bg-secondary text-white px-3 py-3.5 md:px-3.75 md:py-2.25 lg:px-6 lg:py-3.75 text-xs lg:text-sm font-bold uppercase ${ className }`
 
 	// Render the component with all props
 	return <Component className={ combined_class_name } { ...props }>
