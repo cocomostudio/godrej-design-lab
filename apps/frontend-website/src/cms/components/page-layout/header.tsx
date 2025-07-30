ebdfd32144547aaa299f285a6a00784d0b998964
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-07-30 14:27:25 +0530

    BUG: Frontend website: Broken layout: Adapt everything to work with new container logic

--- apps/frontend-website/src/cms/components/page-layout/header.tsx
+++ apps/frontend-website/src/cms/components/page-layout/header.tsx
@@ -129,27 +129,27 @@ interface HeaderNavigationProps extends React.ComponentProps<"header"> {
 	onClose: () => void;
 }
 function HeaderNavigation ( { navigation, featured_links, isVisible = false, onClose = no_op, className = "" }: HeaderNavigationProps ) {
-	return <header className={ `bg-primary relative after:hidden md:after:block after:absolute after:top-0 after:right-0 after:w-1/3 after:h-full md:after:bg-secondary ${ className }` }>
-		<nav className="md:container md:grid-layout">
+	return <header className={ `bg-primary relative after:hidden md:after:block after:absolute after:top-0 after:right-0 after:w-1/3 after:h-full md:after:bg-secondary overflow-hidden ${ className }` }>
+		<nav className="container md:grid-layout">
 			<div className="md:hidden end-col-1 text-secondary">
-				<div className="container flex justify-between items-start pt-8">
+				<div className="flex justify-between items-start pt-8">
 					<Link to={ "/" } className={ `transition-opacity duration-250 ease-out ${ isVisible ? 'opacity-100 delay-300' : 'opacity-0' }` }>
 						<GDLLogo className="w-1c md:w-[calc(var(--column-width)/2)] lg:w-[calc(3*(var(--column-width)/4))] h-auto fill-secondary" />
 					</Link>
 					<div className={ `w-full transition-opacity duration-150 ease-out ${ isVisible ? "opacity-100 delay-300" : "opacity-0" }` }>
-						<div className="container text-right">
+						<div className="text-right">
 							<button type="button" className="text-sm font-bold uppercase" onClick={ onClose }>Close</button>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div className={ `start-col-2 end-gutter-5 lg:end-col-7 text-secondary transition-opacity duration-250 ease-out ${ isVisible ? "opacity-100 delay-300" : "opacity-0" }` }>
-				<ul className="mt-8 container flex flex-col md:flex-row flex-wrap gap-6 pb-6 md:gap-3 lg:gap-1g md:pb-0">
+				<ul className="mt-8 flex flex-col md:flex-row flex-wrap gap-6 pb-6 md:gap-3 lg:gap-1g md:pb-0">
 					{ navigation.map( ( { label, url }, i ) => <li key={ i } className="md:w-1c-3g lg:w-2c-1g border-t border-secondary border-solid pt-6 md:pt-3"><Link to={ url } className="text-p md:text-sm font-bold uppercase">{ label }</Link></li> ) }
 				</ul>
 			</div>
 			<div className="start-col-6 end-col-last md:ml-1g md:pl-6 lg:start-col-8 lg:ml-[calc(var(--column-width)/2)] bg-secondary text-primary z-10">
-				<div className="container py-8 bg-secondary">
+				<div className="relative py-8 bg-secondary before:absolute before:top-0 before:right-full before:w-1/2 before:h-full before:bg-secondary md:before:hidden after:absolute after:top-0 after:left-full after:w-1/2 after:h-full after:bg-secondary md:after:hidden">
 					<h2 className={ `border-t border-primary border-solid md:border-none pt-4 md:pt-0 text-p font-bold uppercase transition-opacity duration-250 ease-out ${ isVisible ? "opacity-100 delay-300" : "opacity-0" }` }>Latest</h2>
 					<ul className={ `mt-6 space-y-3 transition-opacity duration-250 ease-out ${ isVisible ? "opacity-100 delay-300" : "opacity-0" }` }>
 						{ featured_links.map( ( { label, url }, i ) => <li key={ i } className=""><Link to={ url } className="text-sm">{ label }</Link></li> ) }
