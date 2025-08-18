3b203432faccb02ac721a2b9e4fb6c0bbada01e2
adi <8440761+nostalgicdodo@users.noreply.github.com>
2025-08-18 16:33:55 +0530

    BUG: Heading + Content List: Remove the left margin on the content block

--- apps/frontend-website/src/cms/components/gdl/heading-and-content-list.tsx
+++ apps/frontend-website/src/cms/components/gdl/heading-and-content-list.tsx
@@ -25,7 +25,7 @@ export class HeadingAndContentList {
 			{ content.map( ({ heading, content }, i ) => <li key={ i } className="mt-6 md:mt-8 lg:mt-10 first:mt-0 md:flex md:gap-1g border-b border-black/30 last:border-0 pb-6 md:pb-8 lg:pb-10 last:pb-0">
 				<H className="md:w-2c-1g lg:w-3c-2g text-h4 font-bold uppercase text-secondary" dangerouslySetInnerHTML={{ __html: heading.replace( NEW_LINES_REGEX, html_line_break ) }}></H>
 
-				<div className="max-md:mt-6 md:ml-1g md:w-5c-4g lg:w-6c-5g [&>:first-child]:mt-0 max-md:empty:hidden">
+				<div className="max-md:mt-6 md:w-5c-4g lg:w-6c-5g [&>:first-child]:mt-0 max-md:empty:hidden">
 					<WYSIWYG.Renderer font_family="sans-serif" content={ content } />
 				</div>
 			</li> ) }
