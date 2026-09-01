7c0e92e5555919ac6af04b9fe13db428177993d6
adi <8440761+nostalgicdodo@users.noreply.github.com>
2026-09-02 01:21:43 +0530

    frontend website: quote block: have it occupy just 5 columns, not 6
    We cannot control where quotes can be inserted. Hence, there are
    situations where quotes spill over other content.

--- apps/frontend-website/src/__lib/react/quote.tsx
+++ apps/frontend-website/src/__lib/react/quote.tsx
@@ -18,7 +18,7 @@ export function Quote ( {
 	children,
 	className = "",
 }: ComponentProps<"blockquote"> ) {
-	return <blockquote className={ `mt-6 md:mt-8 lg:mt-10 md:-ml-1c-1g lg:-ml-2c-2g _md:ml-1c-1g _lg:ml-1c-1g md:py-10 lg:py-20 ${ className }` }>
+	return <blockquote className={ `mt-6 md:mt-8 lg:mt-10 md:py-10 lg:py-20 ${ className }` }>
 		<p className="text-h3 md:text-h2 lg:text-h2 font-bold text-secondary">
 			{ children }
 		</p>
